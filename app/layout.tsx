48ade272a68bb15333e74f209c4b44178cfa159c
DariusLukasukas <64962012+DariusLukasukas@users.noreply.github.com>
2023-10-29 13:42:59 +0200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google"
 import "./globals.css"
 import Script from "next/script"
+import { Analytics } from "@vercel/analytics/react"
 import { NextThemesProvider } from "@/components/providers/NextThemesProvider"
 import { CodeIcon, HeartIcon } from "@radix-ui/react-icons"
 import Navigation from "@/components/Navigation"
@@ -43,6 +44,7 @@ export default function RootLayout({
           strategy={"beforeInteractive"}
           src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&callback=Function.prototype`}
         />
+        <Analytics />
       </body>
     </html>
   )
